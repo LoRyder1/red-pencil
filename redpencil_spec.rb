b60e4a5a2365ba42d5a94639015e6c5743e9b2be
Lovinder Pnag <lspnag@gmail.com>
2016-01-25 03:56:12 -0500

    first test for promo

--- redpencil_spec.rb
+++ redpencil_spec.rb
@@ -1,5 +1,13 @@
 require_relative 'redpencil'
 
 describe RedPencil do
-  
+  before(:each) do 
+    @promotion = RedPencil.new(100,80)
+  end
+
+  describe 'promotion begin' do
+    it 'because of reduction of price' do
+      expect(@promotion.promo?).to eq true
+    end
+  end
 end
