de57963143e7855b12c8ce1f268c95248aa13aae
Lovinder Pnag <lspnag@gmail.com>
2016-01-25 04:54:14 -0500

    percent off test passing

--- redpencil.rb
+++ redpencil.rb
@@ -5,6 +5,15 @@ class RedPencil
   end
 
   def promo?
-    @price > @promo_price
+    @price > @promo_price && percent_off > 5
   end
+
+  def percent_off
+    (@price - @promo_price)/@price.to_f * 100
+  end
+
 end
+
+x = RedPencil.new(100,80)
+
+# p x.percent_off
