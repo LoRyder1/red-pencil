6999395e2981d05b3087350a5188e1e10ac49bea
Lovinder Pnag <lspnag@gmail.com>
2016-01-25 05:12:08 -0500

    test for promo check less than 30&

--- redpencil_spec.rb
+++ redpencil_spec.rb
@@ -3,6 +3,8 @@ require_relative 'redpencil'
 describe RedPencil do
   before(:each) do 
     @promotion = RedPencil.new(100,80)
+    @promotion1 = RedPencil.new(100,98)
+    @promotion2 = RedPencil.new(100,68)
   end
 
   describe 'promotion begin' do
@@ -11,8 +13,11 @@ describe RedPencil do
     end
 
     it 'with at least 5% reduction?' do
-      @promotion = RedPencil.new(100,98) 
-      expect(@promotion.promo?).to eq false
+      expect(@promotion1.promo?).to eq false
+    end
+
+    it 'but no more than 30% reduction' do
+      expect(@promotion2.promo?).to eq false
     end
   end
 end
